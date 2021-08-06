9eb4ad23e99d34529ff1eb3ec02cf4bb76bdf9be
renatoDomingues <kungfurenato30@gmail.com>
2021-08-05 21:12:54 -0300

    05/08/2021

--- mongo.js
+++ mongo.js
@@ -5,15 +5,15 @@ mongoose.Promise = global.Promise
 
 mongoose
     //.connect('mongodb://localhost/mongo-orm', {useMongoClient: true})
-    .connect('mongodb://localhost/mongo-orm', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(()=>{
+    .connect('mongodb+srv://mongo-orm:mongo-orm@mongo-orm.ix1gq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
         const PessoaSchema = mongoose.Schema({
             nome: String,
             cargo: String
         })
         const Pessoa = mongoose.model('Pessoa', PessoaSchema)
         const renato = new Pessoa({nome: 'Renato Domingues', cargo: 'CTO'})
-        renato.save(()=>console.log('salvo'))
+        renato.save( () => console.log('salvo'))
 /*
         Pessoa.find({}, (err, docs)=>{
             console.log(docs)
